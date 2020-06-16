bb8d16339a421158d432c7c5dc0710d29ae6a8ae
Song <Song@DESKTOP-NUUA4ES>
2020-06-17 02:01:36 +0800

    Add project files.

---
+++ SqlConnection.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using SqlConnection.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlConnection.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new SqlConnectionFluent("connectionstring")
+                .UseStoredProcedure()
+                .SetTimeOut(5 * 60)
+                .SetParameter("a_id", 123)
+                .SetParameter("b_id", "string")
+                .ExecuteQuery<QueryResult>();
+
+            foreach(var item in result)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+
+    class QueryResult
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+        public int other_id { get; set; }
+    }
+}
---
+++ SqlConnection.Fluent/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlConnection.Fluent
+{
+    public class SqlConnectionFluent
+    {
+        private string _connectionString;
+        private CommandType _commandType;
+        private int _timeOut;
+        private List<KeyValuePair<string, object>> parameters = new List<KeyValuePair<string, object>>();
+
+        public SqlConnectionFluent(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SqlConnectionFluent UseStoredProcedure()
+        {
+            _commandType = CommandType.StoredProcedure;
+            return this;
+        }
+
+        public SqlConnectionFluent SetTimeOut(int timeout)
+        {
+            _timeOut = timeout;
+            return this;
+        }
+
+        public SqlConnectionFluent SetParameter(string parameter, object value)
+        {
+            parameters.Add(new KeyValuePair<string, object>(parameter, value));
+            return this;
+        }
+
+        public List<T> ExecuteQuery<T>()
+            where T : new()
+        {
+            var ts = new List<T>();
+            using (var connection = new System.Data.SqlClient.SqlConnection(_connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandType = _commandType;
+                command.CommandTimeout = _timeOut;
+                foreach(var parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+
+                var properties = typeof(T).GetProperties().ToList();
+                connection.Open();
+                using(var reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        var t = new T(); 
+                        foreach (var property in properties)
+                        {
+                            var row = reader.GetValue(reader.GetOrdinal(property.Name));
+                            var propertyInfo = t.GetType().GetProperty(property.Name);
+                            propertyInfo.SetValue(propertyInfo, row);
+                        }
+                        ts.Add(t);
+                    }
+                }
+            }
+            return ts;
+        }
+        
+    }
+}
